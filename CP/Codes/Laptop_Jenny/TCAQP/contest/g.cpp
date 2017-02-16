2901a603a00822b008fae3ac65b7020dcddcb491
Boxer <luisv0909@gmail.com>
2017-02-16 12:52:59 -0500

    fifth commit

--- CP/Codes/Laptop_Jenny/TCAQP/contest/g.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int main(){
-	cout << 1 ;
-	for(int i=0;i<100000;i++)
-		cout<< 0;
-}
--- CP/Codes/Laptop_Jenny/luis/1639_TI.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-	int n , m;
-	while( cin >> n >> m ){
-		puts( !( ( n * m ) & 1 ) ? "[:=[first]": "[second]=:]" );
-	}
-}
